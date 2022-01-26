6bd58840c8341b372c31e95295c1f113b26b2787
babywkb <baby_hiroki_check@yahoo.co.jp>
2022-01-26 18:39:27 +0900

    スクリプト作成

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+//only for iOS
+
+// scraping hash information form "接触日シート別冊"
+puppeteer.launch().then(async (browser) => {
+  // const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://datastudio.google.com/u/0/reporting/069598a2-3f01-4b51-b023-cdb478992182/page/blffB",
+    { waitUntil: "networkidle2" }
+  );
+
+  const hashInfos = await page.evaluate(() => {
+    const rows = document.querySelectorAll("div.row");
+    const infos = Array.from(rows, (node) => {
+      const possibleDate = node.children[3].title;
+      const hash = node.children[4].title;
+      // hashInfo Type
+      return { possibleDate: possibleDate, hash: hash };
+    });
+    return infos;
+  });
+
+  await browser.close();
+
+  // arrage local JSON file
+  const myExposureJson = JSON.parse(
+    fs.readFileSync(`./data/${process.argv[2]}`, "utf8")
+  );
+  const myExposureChecks = myExposureJson.ExposureChecks.map((check) => check);
+
+  const hashCompareDates = myExposureChecks.map((check) => {
+    const timestamp = check.Timestamp;
+    const hashes = check.Files.filter((file) => file.MatchCount > 0).map(
+      (file) => file.Hash
+    );
+
+    // hashCompareDate Type
+    return { timestamp: timestamp, hashes: hashes };
+  });
+
+  const result = hashCompareDates.map((compareDate) => {
+    const exposures = compareDate.hashes.map((myHash) => {
+      const matchInfo = hashInfos.find((info) => myHash === info.hash);
+      if (matchInfo) {
+        return { 接触推定日: matchInfo.possibleDate, HASH値: myHash };
+      }
+      return { 接触推定日: " 一致データなし", HASH値: myHash };
+    });
+    return {
+      HASH突合日: compareDate.timestamp,
+      接触: exposures,
+    };
+  });
+
+  console.log(JSON.stringify(result, null, '\t'));
+});
